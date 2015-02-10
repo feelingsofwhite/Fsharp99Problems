8a668251e2d9485510dd1eb676f8f789b3b51253
James Keller <james@feelingsofwhite.com>
2015-02-09 20:38:19 -0700

    euler 3, but really all i've done is start exploring FsCheck

---
+++ euler/3/Program.fs
@@ -0,0 +1,31 @@
+﻿//
+// The prime factors of 13195 are 5, 7, 13 and 29.
+//
+// What is the largest prime factor of the number 600851475143 ?
+//
+
+
+//explore FsCheck native (ie no xUnit.Net integration
+
+open FsCheck
+
+let revRevIsOrig (xs:list<int>) = List.rev(List.rev xs) = xs
+
+Check.Quick revRevIsOrig
+
+let revIsOrigBad (xs:list<int>) = List.rev xs = xs
+
+Check.Quick revIsOrigBad
+
+
+
+
+//boilerplate
+
+[<EntryPoint>]
+let main argv = 
+    printfn "args are %A" argv
+    if (System.Diagnostics.Debugger.IsAttached) then
+        printfn "[press any key]"
+        System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
